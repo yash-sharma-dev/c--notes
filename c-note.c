16d8f1fe9c173220bedeaef5ebc440b4272c3b67
yash-sharma-dev <yashbonzer.work@gmail.com>
2024-08-11 21:19:04 +0530

    full notes completed

--- c-note.c
+++ c-note.c
@@ -735,6 +735,188 @@ int main()
 
 //#32 enums
 
+enum Day{Sun = 1, Mon = 2, Tue = 3, Wed = 4, Thu = 5, Fri = 6, Sat = 7};
+
+int main()
+{
+   // enum = a user defined type of named integer identifiers
+   //        helps to make a program more readable
+
+   enum Day today;
+   today = Sun;
+
+   if(today == Sun || today == Sat)
+   {
+      printf("\nIt's the weekend! Party time!");
+   }
+   else
+   {
+      printf("\nI have to work today :(");
+   }
+ 
+   return 0;
+}
+
+//#33 random numbers genration
+
+int main()
+{
+   //pseudo random numbers = A set of values or elements that is statistically random
+   //                        (Don't use these for any sort of cryptographic security)
+ 
+   // Use current time as a seed for random # generator
+   srand(time(0));
+ 
+   // rand() generates a pseudo random # between 0 - 32,767
+   int number1 = (rand() % 6) + 1;
+   int number2 = (rand() % 6) + 1;
+   int number3 = (rand() % 6) + 1;
+ 
+   printf("%d\n", number1);
+   printf("%d\n", number2);
+   printf("%d\n", number3);
+ 
+   return 0;
+}
+
+//#34 bitwise opretors 
+
+
+int main()
+{
+   // BITWISE OPERATORS = special operators used in bit level programming
+   //                                          (knowing binary is important for this topic)
+
+   // & = AND
+   // | = OR
+   // ^ = XOR
+   // <<  left shift
+   // >>  right shift
+
+   int x = 6;  //    6 = 00000110
+   int y = 12; // 12 = 00001100 
+   int z = 0;  //    0 = 00000000
+
+   z = x & y;
+   printf("AND = %d\n", z);
+
+   z = x | y;
+   printf("OR = %d\n", z);
+
+   z = x ^ y;
+   printf("XOR = %d\n", z);
+
+   z = x << 2;
+   printf("SHIFT LEFT = %d\n", z);
+
+   z = x >> 2;
+   printf("SHIFT RIGHT = %d\n", z);
+
+   return 0;
+}
+
+//#35 memory adresses 
+
+int main()
+{
+   // memory = an array of bytes within RAM (street)
+   // memory block = a single unit (byte) within memory (house), used to hold some value (person)
+   // memory address = the address of where a memory block is located (house address)
+
+   char a;
+   char b[1];
+
+   printf("%d bytes\n", sizeof(a));
+   printf("%d bytes\n", sizeof(b));
+
+   printf("%p\n", &a);
+   printf("%p\n", &b);
+
+   return 0;
+}
+
+//#36 pointers 
+
+void printAge(int *pAge)
+{
+   printf("You are %d years old\n", *pAge); //dereference
+}
+
+int main()
+{
+   // pointer = a "variable-like" reference that holds a memory address to another variable, array, etc.
+   //           some tasks are performed more easily with pointers
+   //           * = indirection operator (value at address)
+
+   int age = 21;
+   int *pAge = &age;
+
+   printAge(pAge);
+
+   //printf("address of age: %p\n", &age);
+   //printf("value of pAge: %p\n", pAge);
+   //printf("size of age: %d bytes\n", sizeof(age));
+   //printf("size of pAge: %d bytes\n", sizeof(pAge));
+   //printf("value of age: %d\n", age);
+   //printf("value at stored address: %d\n", *pAge); //dereferencing
+
+   return 0;
+}
+
+//#37 writing files in C
+
+int main()
+{
+   // WRITE/APPEND A FILE
+   FILE *pF = fopen("C:\\Users\\Cakow\\Desktop\\test.txt", "w");
+
+   fprintf(pF, "Spongebob Squarepants");
+
+   fclose(pF);
+   
+   // DELETE A FILE
+   /*
+   if(remove("test.txt") == 0)
+   {
+      printf("That file was deleted successfully!");
+   }
+   else
+   {
+      printf("That file was NOT deleted!");
+   }
+   */
+   return 0;
+}
+
+
+//#38 read files in C
+
+int main()
+{
+   FILE *pF = fopen("poem.txt", "r");
+   char buffer[255];
+
+   if(pF == NULL)
+   {
+      printf("Unable to open file!\n");
+   }
+   else
+   {
+      while(fgets(buffer, 255, pF) != NULL)
+      {
+         printf("%s", buffer);
+      }
+   }
+
+   fclose(pF);
+
+   return 0;
+}
+
+
+
+/*THE END*/
+
 
 
 
